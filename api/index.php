913fe5341a2914a71a2245d25355ea56731bf19d
syhadr <syahruladrianto444@gmail.com>
2026-04-17 00:34:05 +0700

    temp: hide package.json to force PHP-only build on Vercel

---
+++ api/index.php
@@ -0,0 +1,3 @@
+<?php
+// Vercel Entry Point
+require __DIR__ . '/../public/index.php';
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (isset($_SERVER['VERCEL_URL'])) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
